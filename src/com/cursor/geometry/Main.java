8bb3c6f0e946c1430339d6610dd243fcf43faf4a
Kateryna-Chernovol <schasliva@yahoo.com>
2021-10-07 23:27:46 +0300

    HW6 initial commit

---
+++ src/com/cursor/geometry/Main.java
@@ -0,0 +1,61 @@
+package com.cursor.geometry;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("1. Remove divisible by 3");
+        List<Integer> array = new ArrayList<>();
+        array.add(3);
+        array.add(9);
+        array.add(11);
+        array.add(18);
+        array.add(20);
+        array.add(22);
+
+        for (Integer value : array) {
+            System.out.print(value + "  ");
+        }
+        System.out.println();
+        array.removeIf(x -> x % 3 == 0);
+
+        for (Integer value : array) {
+            System.out.print(value + "  ");
+        }
+        System.out.println("\n");
+
+        System.out.println("2. Swap \"Orange\" & \"Grapefruit\" if present");
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Orange");
+        fruits.add("Apple");
+        fruits.add("Grapefruit");
+        System.out.println(fruits);
+        int aElement = fruits.indexOf("Orange");
+        int bElement = fruits.indexOf("Grapefruit");
+        //
+        if (fruits.contains("Orange") && fruits.contains("Grapefruit")) {
+            Collections.swap(fruits, aElement, bElement);
+        } else {
+            System.out.println("Value not present");
+        }
+        System.out.println(fruits + "\n");
+
+        System.out.println("3. Check similar elements in two ArrayLists");
+        List<Integer> arrayFirst = new ArrayList<>();
+        arrayFirst.add(10);
+        arrayFirst.add(12);
+        arrayFirst.add(15);
+        System.out.println("First ArrayList: " + arrayFirst);
+        List<Integer> arraySecond = new ArrayList<>();
+        arraySecond.add(15);
+        arraySecond.add(18);
+        arraySecond.add(12);
+        System.out.println("Second ArrayList: " + arraySecond);
+        arrayFirst.retainAll(arraySecond);
+        System.out.println("Same values: " + arrayFirst);
+    }
+}
